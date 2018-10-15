eb43fd16d2aaaa5ac3463b150fbf5ac51d4f7b81
jeffchen <513100163@qq.com>
2018-10-15 19:40:13 +0800

    实现strStr()函数

---
+++ 实现strStr.py
@@ -0,0 +1,14 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+        else:
+            for i in range(len(haystack)-len(needle)+1):
+                if haystack[i:i+len(needle)] == needle:
+                    return i
+            return -1

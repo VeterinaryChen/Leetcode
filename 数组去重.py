9a9d35850ac4087beb1a466b7786cc72a94866a6
jeffchen <513100163@qq.com>
2018-10-15 19:43:50 +0800

    使用python实现数组去重

---
+++ 数组去重.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        while i<len(nums)-1:
+            if nums[i]==nums[i+1]:
+                nums.remove(nums[i])
+            else:
+                i+=1
+        return len(nums)

3dde324fcaeeb0aad51a5edffb0a63b85f8f9f4d
Jeff Chen <40842903+VeterinaryChen@users.noreply.github.com>
2019-03-09 19:51:40 +0800

    Create 27.移除元素.java

---
+++ 27.移除元素.java
@@ -0,0 +1,19 @@
+class Solution {
+   public int removeElement(int[] nums, int val) {
+       int count=0;
+       if(nums.length==0){
+           return 0;
+       }
+       if (nums.length==1 && nums[0]==val){
+           return 0;
+       }
+
+       for (int i=0 ; i<nums.length ; i++){
+           if(nums[i]!=val){
+               nums[count] = nums[i];
+               count++;
+           }
+       }
+       return count;
+   }
+ }
